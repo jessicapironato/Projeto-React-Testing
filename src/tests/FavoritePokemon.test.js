20d50e8e96b17fa77460f3cef65798f249bc70bd
Jéssica <jessicapmaximo@gmail.com>
2023-02-27 16:56:00 -0300

    falta 1 teste

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemon';
+
+describe('Teste o componente FavoritePokemon.js', () => {
+  test('1.É exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const notFoundText = screen.getByText(/No favorite Pokémon found/i);
+    expect(notFoundText).toBeVisible();
+  });
+
+  test('2.São exibidos na tela apenas os Pokémon favoritados', () => {
+
+  });
+});
