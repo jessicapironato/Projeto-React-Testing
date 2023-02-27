aeaf3f571246f18a0747535d9a0a20e35ead655a
Jéssica <jessicapmaximo@gmail.com>
2023-02-27 17:43:08 -0300

    falta 1

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('1.É exibido na tela um h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const title = screen.getByRole('heading', { name: /Page requested not found/i, level: 2 });
+
+    expect(title).toBeInTheDocument();
+  });
+});
