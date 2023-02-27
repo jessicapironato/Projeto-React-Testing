9d62390d75bf575c0a4172ab4adf8d196af870e3
Jéssica <jessicapmaximo@gmail.com>
2023-02-27 17:29:57 -0300

    requisito 3 ok

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import FavoritePokemon from '../pages/FavoritePokemon';
+import App from '../App';
 
 describe('Teste o componente FavoritePokemon.js', () => {
   test('1.É exibido na tela a mensagem No favorite pokemon found', () => {
@@ -12,6 +14,21 @@ describe('Teste o componente FavoritePokemon.js', () => {
   });
 
   test('2.São exibidos na tela apenas os Pokémon favoritados', () => {
+    const { history } = renderWithRouter(<App />);
 
+    act(() => {
+      history.push('/pokemon/25');
+    });
+
+    const favoritePokemonInput = screen.getByRole('checkbox');
+    expect(favoritePokemonInput).toBeVisible();
+    userEvent.click(favoritePokemonInput);
+
+    act(() => {
+      history.push('/favorites');
+    });
+
+    const favoritePokemon = screen.getByText('Pikachu');
+    expect(favoritePokemon).toBeVisible();
   });
 });
